cbf26f37752447c36ffa6607e3ba690a2fbe3be4
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-09-16 20:19:12 +0500

    implement functions to make request to api

---
+++ src/api-services/services.js
@@ -0,0 +1,21 @@
+import http from '../constants/httpCommon';
+
+export const getCars = () => http.get('/cars');
+
+export const getCategories = () => http.get('/categories');
+
+export const getManufacturers = () => http.get('/manufacturers');
+
+export const getBookings = () => http.get('/bookings');
+
+export const addCar = car => http.post('/cars', car);
+
+export const addCategory = category => http.post('/category', category);
+
+export const addManufacturer = manufacturer => http.post('/manufacturers', manufacturer);
+
+export const signup = user => http.post('/signup', user);
+
+export const login = user => http.post('/login', user);
+
+export const loggedIn = () => http.get('logged_in');
