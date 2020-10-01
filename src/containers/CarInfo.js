c7bf2ce48b4350a259e5a8ff21fe5aa08af5cbca
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-10-01 22:44:54 +0500

    add booking component and container

--- src/containers/CarInfo.js
+++ src/containers/CarInfo.js
@@ -1,18 +1,53 @@
-import React, { useEffect } from 'react';
+/* eslint-disable camelcase */
+import React, { useState, useEffect } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { withRouter, useParams } from 'react-router';
 import PropTypes from 'prop-types';
 import { fetchCarInfo } from '../actions/getActions';
+import { bookCar } from '../actions/userActions';
 
 const CarInfo = ({ car }) => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const [bookingData, setBookingData] = useState();
+  const {
+    user_id,
+    car_id,
+    start_date,
+    end_date,
+  } = bookingData;
 
   useEffect(() => {
     fetchCarInfo(parseInt(id, 10))(dispatch);
   }, [id, dispatch]);
 
-  console.log('car obj', car);
+  const handleBookingChange = e => {
+    const { name, value } = e.target;
+
+    setBookingData({
+      ...bookingData,
+      [name]: value,
+    });
+  };
+
+  const handleBookingSubmit = e => {
+    e.preventDefault();
+    const booking = {
+      user_id,
+      car_id,
+      start_date,
+      end_date,
+    };
+
+    bookCar(booking)(dispatch);
+  };
+
+  const isFutureDate = idate => {
+    const today = new Date().getTime();
+    idate = idate.split('/');
+    idate = new Date(idate[2], idate[1] - 1, idate[0]).getTime();
+    return (today - idate) < 0;
+  };
 
   const isCarId = true ? car.id === parseInt(id, 10) : false;
 
@@ -58,6 +93,22 @@ const CarInfo = ({ car }) => {
                   {car.description}
                 </p>
               </div>
+            </div>
+            <div>
+              Book car
+              <form onSubmit={handleBookingSubmit}>
+                <input type="hidden" name="user_id" value={user_id} />
+                <input type="hidden" name="car_id" value={car_id} />
+                <label htmlFor="start-date">
+                  Start Date:
+                  <input type="date" id="start-date" name="start_date" value={start_date} onChange={handleBookingChange} />
+                </label>
+                <label htmlFor="end-date">
+                  End Date:
+                  <input type="date" id="end-date" name="end_date" value={end_date} onChange={handleBookingChange} />
+                </label>
+                <input type="submit" value="Book car" />
+              </form>
             </div>
           </div>
         ) : (
