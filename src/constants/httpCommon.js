ef0b51b2dac522176f8e9ccd2b94718628c44a3e
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-10-06 11:29:36 +0500

    base url

--- src/constants/httpCommon.js
+++ src/constants/httpCommon.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
+  // http://localhost:3000
+  // https://car-booking-api-app.herokuapp.com
   baseURL: 'https://car-booking-api-app.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
